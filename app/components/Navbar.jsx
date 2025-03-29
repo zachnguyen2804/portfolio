085d09ff20351f7c73a4063fb69d160b01f1d50a
zachnarry <qung5100@colorado.edu>
2025-03-29 10:49:34 +0700

    initial commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useRef, useState } from "react";
+
+export default function Navbar() {
+  const sideMenuRef = useRef();
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
+
+  return (
+    <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-enter justify-between z-50">
+        <a href="#top">
+          <Image
+            src={assets.logo}
+            className="w-28 cursor-pointer mr-14"
+            alt=""
+          />
+        </a>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50 ">
+          <li>
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about">
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#work">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact">
+              Contact me
+            </a>
+          </li>
+        </ul>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={assets.moon_icon} alt="" className="w-6" />
+          </button>
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
+          >
+            Contact <Image src={assets.arrow_icon} alt="" className="w-3" />
+          </a>
+
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+            <Image src={assets.menu_black} alt="" className="w-6" />
+          </button>
+        </div>
+
+        {/* mobile menu */}
+        <ul
+          ref={sideMenuRef}
+          className="flex flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 md:hidden"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
+          <li>
+            <a className="font-Ovo" href="#top" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about" onClick={closeMenu}>
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#services" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#work" onClick={closeMenu}>
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact" onClick={closeMenu}>
+              Contact me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+// tailwind.config.mjs
+// import { type Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
